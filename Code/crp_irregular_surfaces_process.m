c5d6cb0153690248b6027e18aaef21f2d5fce194
Phil Dixon <PhilD001@users.noreply.github.com>
2021-02-03 09:40:22 -0500

    add run instructions, total trial counter

--- Code/crp_irregular_surfaces_process.m
+++ Code/crp_irregular_surfaces_process.m
@@ -1,18 +1,22 @@
 function crp_irregular_surfaces_process
-% This code computes continuous relative phase metrics for the uneven walking surface dataset. 
+% This code computes continuous relative phase metrics for the uneven walking surface
+% dataset. 
+
+% INSTRUCTIONS: See README.md for detailed running instructions
+
+% Last updated by Philippe C. Dixon Feb 3 2021
 
 
+%% STEP 0: SET DEFAULTS =============================================================
 
-%% STEP 0: SET DEFAULTS ============================================================================
-
-% initial path to data folder ----------------------------------------------------------------------
+% initial path to data folder -------------------------------------------------------
 [p, ~] = fileparts(mfilename('fullpath'));
 indx = strfind(p, filesep);
 fld_data_root = [p(1:indx(end)), 'Data', filesep, 'raw'];
 fld_stats = [p(1:indx(end)), 'Statistics'];
 
 
-% create copy of data for processing ---------------------------------------------------------------
+% create copy of data for processing ------------------------------------------------
 indx = strfind(fld_data_root, filesep);
 fld_data_proc = [fld_data_root(1:indx(end)), 'processed'];
 
@@ -26,11 +30,12 @@ mkdir(fld_data_proc);
 copyfile(fld_data_root,fld_data_proc)
 
 
-%% STEP 1: COMPUTE CRP FOR EACH TRIAL ==============================================================
+%% STEP 1: COMPUTE CRP FOR EACH TRIAL ===============================================
 
 limb = {'Right', 'Left'};
 group = {'Old', 'Young'};
 surface = {'Flat', 'Uneven'};
+n_trials = 0;
 for g = 1:length(group)
     
     for s = 1:length(surface)
@@ -38,14 +43,16 @@ for g = 1:length(group)
         participant = GetSubDirsFirstLevelOnly([fld_data_proc, filesep, group{g}]);
         for p = 1:length(participant)
             
-            fld_gsp = [fld_data_proc, filesep, group{g}, filesep, participant{p}, filesep, surface{s}];
+            fld_gsp = [fld_data_proc, filesep, group{g}, filesep, participant{p},...
+                       filesep, surface{s}];
             fl = engine('fld', fld_gsp, 'extension', 'zoo');
             
             disp(' ')
-            disp(['extracting data from ', num2str(length(fl)), ' trials for subject ', ...
-                 participant{p}, ' on surface ', surface{s}, ' ...'])
+            disp(['extracting data from ', num2str(length(fl)), ...
+                  ' trials for subject ', participant{p}, ' on surface ', ...
+                  surface{s}, ' ...'])
 
-            % initialize stk for each CRP metric for a given group/surface/participant -------------
+            % initialize stk for each CRP metric by group/surface/participant -------
             RightKHStanceCRP_stk = ones(length(fl), 101);     
             RightKHSwingCRP_stk = ones(length(fl), 101);      
             
@@ -60,17 +67,17 @@ for g = 1:length(group)
             
             vel_stk = ones(length(fl), 1);     
 
-            % loop through all trials for a given group/surface/participant ------------------------
+            % loop through all trials for a given group/surface/participant ---------
             for f = 1:length(fl)
                 [file_pth, file_name, ext] = fileparts(fl{f});
                 disp(file_name)
                 data = load(fl{f}, '-mat');
                 data = data.data;
                 
-                % Checks which limb side is associated with the middle step (apex) -----------------
+                % Checks which limb side is associated with middle step (apex) ------
                 ApexFoot=data.zoosystem.CompInfo.ApexFoot;
                                 
-                % Extracts Stance and Swing phase indices based on existing gait events ------------
+                % Extracts Stance / Swing indices based on existing gait events -----
                 FSapex = data.SACR.event.FSapex(1);      % middle portion
                 FSminus1 = data.SACR.event.FSminus1(1);  % step before apex
                 FSplus1 = data.SACR.event.FSplus1(1);    % step after apex
@@ -83,21 +90,22 @@ for g = 1:length(group)
                     Swing = FSminus1(1):FSapex(1);
                 end
                 
-                % extract SACR position and compute speed ------------------------------------------
+                % extract SACR position and compute speed ---------------------------
                 SACR = data.SACR.line(FSminus1:FSplus1, :);  
-                SACR_mag = sqrt(SACR(:,1).* SACR(:,1) + SACR(:,2).* SACR(:,2) + SACR(:,3).* SACR(:,3));
-                SACR_mag = SACR_mag/1000;                             % convert to meters
-                time = length(SACR_mag)/data.zoosystem.Video.Freq;    % get time from frequecy
-                vel_stk(f) = abs((SACR_mag(end)-SACR_mag(1))/time);   % velocity in m/s
+                SACR_mag = sqrt(SACR(:,1).* SACR(:,1) + SACR(:,2).* SACR(:,2) + ...
+                                SACR(:,3).* SACR(:,3));
+                SACR_mag = SACR_mag/1000;                             % convert to m
+                time = length(SACR_mag)/data.zoosystem.Video.Freq;    % freq to time
+                vel_stk(f) = abs((SACR_mag(end)-SACR_mag(1))/time);   % velocity m/s
                 
                 for l = 1:length(limb)
                     
-                    % Extract Joint Angles ---------------------------------------------------------
+                    % Extract Joint Angles ------------------------------------------
                     Hip = data.([limb{l}, 'HipAngle_x']).line;
                     Knee = data.([limb{l}, 'KneeAngle_x']).line;
                     Ankle = data.([limb{l}, 'AnkleAngle_x']).line;
                     
-                    % Hip, knee, and ankle phase angle padded to nearest event on either side ------
+                    % Hip, knee, ankle phase angle padded to nearest event on either side ------
                     HipCyclePhase  = Phase_Angle(Hip);
                     HipStancePhase = HipCyclePhase(Stance);
                     HipSwingPhase  = HipCyclePhase(Swing);
@@ -110,14 +118,14 @@ for g = 1:length(group)
                     AnkleStancePhase = AnkleCyclePhase(Stance);
                     AnkleSwingPhase  = AnkleCyclePhase(Swing);
                     
-                    % CRP calculations -------------------------------------------------------------
+                    % CRP calculations ----------------------------------------------
                     KHStanceCRP = CRP(KneeStancePhase,HipStancePhase);
                     KHSwingCRP  = CRP(KneeSwingPhase,HipSwingPhase);
                     
                     AKStanceCRP = CRP(AnkleStancePhase, KneeStancePhase);
                     AKSwingCRP  = CRP(AnkleSwingPhase, KneeSwingPhase);
                     
-                    % Time Normalizes CRP curves to 100 percent (101 points) -----------------------
+                    % Time Normalizes CRP curves to 100 percent (101 points) --------
                     KHStanceCRP_Norm = TimeNorm(KHStanceCRP, 'spline');
                     KHSwingCRP_Norm  = TimeNorm(KHSwingCRP, 'spline');
                     
@@ -136,14 +144,15 @@ for g = 1:length(group)
                         LeftAKSwingCRP_stk(f, :) = AKSwingCRP_Norm;
                     end
                 end
+                n_trials = n_trials + 1;
             end
             
-            % delete temp files --------------------------------------------------------------------
+            % delete temp files -----------------------------------------------------
             for f = 1:length(fl)
                 java.io.File(fl{f}).delete();
             end
             
-            % compute mean absolute relative phase (MARP) ------------------------------------------
+            % compute mean absolute relative phase (MARP) ---------------------------
             RightKHStance_MARP = mean(RightKHStanceCRP_stk);
             RightKHSwing_MARP  = mean(RightKHSwingCRP_stk);
             RightAKStance_MARP = mean(RightAKStanceCRP_stk);
@@ -154,7 +163,7 @@ for g = 1:length(group)
             LeftAKStance_MARP = mean(LeftAKStanceCRP_stk);
             LeftAKSwing_MARP  = mean(LeftAKSwingCRP_stk);
             
-            % Compute deviation phase (DP) ---------------------------------------------------------
+            % Compute deviation phase (DP) ------------------------------------------
             RightKHStance_DP = std(RightKHStanceCRP_stk);
             RightKHSwing_DP  = std(RightKHSwingCRP_stk);
             RightAKStance_DP = std(RightAKStanceCRP_stk);
@@ -168,7 +177,7 @@ for g = 1:length(group)
             % compute mean velocity
             vel_mean = mean(vel_stk);
             
-            % create new zoo file for ensembled (mean, std) data -----------------------------------
+            % create new zoo file for ensembled (mean, std) data --------------------
             fl_ens = [file_pth, filesep, file_name(1:end-2), 'ens', ext];
             data_ens = struct;
             
@@ -193,7 +202,7 @@ for g = 1:length(group)
             data_ens.LeftAKStance_DP.line =LeftAKStance_DP;
             data_ens.LeftAKSwing_DP.line = LeftAKSwing_DP;
             
-            % compute events at each phase of gait cycle for each MARP, DP curve -------------------
+            % compute events at each phase of gait cycle for each MARP, DP curve ----
             ch = fieldnames(data_ens);            
             for c = 1:length(ch)
                 r = data_ens.(ch{c}).line;
@@ -225,19 +234,18 @@ for g = 1:length(group)
 end
 
 
-%% EXTRACT TRIAL BY TRIAL EVENTS TO SPREADSHEET ====================================================
+%% EXTRACT TRIAL BY TRIAL EVENTS TO SPREADSHEET =====================================
 
-subjects = {'OA03','OA06','OA08','OA10','OA11','OA12','OA14','OA15','OA17','OA18','OA19','OA20',...
-            'OA21','OA22','OA23','OA25','OA26','YA01','YA02','YA03','YA04','YA05','YA06','YA07',...
-            'YA08','YA09','YA10','YA11','YA12','YA13','YA16','YA17','YA18','YA19','YA20'};
+subjects = {'OA03','OA06','OA08','OA10','OA11','OA12','OA14','OA15','OA17','OA18',...
+            'OA19','OA20','OA21','OA22','OA23','OA25','OA26','YA01','YA02','YA03',...
+            'YA04','YA05','YA06','YA07','YA08','YA09','YA10','YA11','YA12','YA13',...
+            'YA16','YA17','YA18','YA19','YA20'};
 cons = {'Old\Flat', 'Old\Uneven', 'Young\Flat', 'Young\Uneven'};
 lcl_evts = {'IC', 'LR', 'MS', 'TS', 'PSw', 'ISw', 'MSw', 'TSw', 'velocity'};
-chns = {'RightKHStance_MARP','RightKHSwing_MARP', 'RightAKStance_MARP','RightAKSwing_MARP', ...
-        'RightKHStance_DP', 'RightKHSwing_DP', 'RightAKStance_DP', 'RightAKSwing_DP'};   
+chns = {'RightKHStance_MARP','RightKHSwing_MARP', 'RightAKStance_MARP',...
+        'RightAKSwing_MARP', 'RightKHStance_DP', 'RightKHSwing_DP', ...
+        'RightAKStance_DP', 'RightAKSwing_DP'};   
 
 eventval('fld', fld_data_proc, 'dim1', cons, 'dim2', subjects, 'ch', chns, ...
          'localevents', lcl_evts, 'globalevents', 'none');
-
-
-
 
